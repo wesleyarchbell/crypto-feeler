3b0705c35458002758e71e8680993db3064ef002
Wesley Archbell <wesleyarchbell@gmail.com>
2020-11-24 20:50:14 +1000

    Initial spring boot skeleton project

---
+++ src/main/java/io/cryptofeeler/app/CryptofeelerApplication.java
@@ -0,0 +1,13 @@
+package io.cryptofeeler.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptofeelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptofeelerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/cryptofeeler/app/CryptofeelerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cryptofeeler.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptofeelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
