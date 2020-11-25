f4ed822c69ccef2ef636aef88f786be4ca6a180f
Wesley Archbell <wesleyarchbell@gmail.com>
2020-11-25 21:04:54 +1000

    Add basic twitter search test

---
+++ src/main/java/io/cryptofeeler/app/feed/twitter/TwitterCoinFeed.java
@@ -0,0 +1,16 @@
+package io.cryptofeeler.app.feed.twitter;
+
+import com.github.redouane59.twitter.TwitterClient;
+
+public class TwitterCoinFeed {
+
+    private TwitterClient twitterClient;
+
+    public TwitterCoinFeed(TwitterClient twitterClient) {
+        this.twitterClient = twitterClient;
+    }
+
+    public void extractData() {
+
+    }
+}
---
+++ src/test/java/io/cryptofeeler/app/feed/twitter/TwitterCoinFeedTest.java
@@ -0,0 +1,32 @@
+package io.cryptofeeler.app.feed.twitter;
+
+import com.github.redouane59.twitter.TwitterClient;
+import com.github.redouane59.twitter.dto.tweet.Tweet;
+import com.github.redouane59.twitter.dto.tweet.TweetSearchResponse;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class TwitterCoinFeedTest {
+
+    private static final String CREDENTIALS_PATH = "src/test/resources/twitter_credentials.json";
+
+    private static TwitterClient twitterClient;
+
+    @BeforeAll
+    public static void before() {
+        System.setProperty("twitter.credentials.file.path", CREDENTIALS_PATH);
+        twitterClient = new TwitterClient();
+    }
+
+    @Test
+    public void shouldExtractBasicFeed() {
+        TweetSearchResponse response = twitterClient.searchForTweetsWithin7days("Bitcoin", 10, null);
+        List<Tweet> tweets = response.getTweets();
+        MatcherAssert.assertThat(tweets.size(), Matchers.equalTo(10));
+    }
+
+}
